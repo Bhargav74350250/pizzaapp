c61f815a6d9822b073d2e55f4b405ab6a65c661b
Bhargav74350250 <110673305+Bhargav74350250@users.noreply.github.com>
2022-09-22 12:29:22 +0530

    Update settings.py

--- PIZZA/settings.py
+++ PIZZA/settings.py
@@ -132,7 +132,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=os.path.join(BASE_DIR,  'staticfiles')
-STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'), )
 
 django_heroku.settings(locals())
